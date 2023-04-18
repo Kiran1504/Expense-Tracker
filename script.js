7224a0d7a99c2e98251fd9d3c73eebe96bea798b
SrirangK <130371151+SrirangK@users.noreply.github.com>
2023-04-18 16:18:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
+var category = document.getElementById('dropdown').innerText
+var selected = document.getElementsByClassName('options')
 function drop(){
     document.getElementById('dropcontent').classList.toggle("show")
 }
 function changecategory(v){
-    let cli = document.getElementById('Drop-head').innerText
-    selected = document.getElementsByClassName('options')
+    
     switch (v) {
         case 1:
-            cli = "Categories : " + selected[v-1].innerHTML
-            document.getElementById('Drop-head').innerHTML =  cli;
+            category = "Categories : " + selected[v-1].innerHTML
+            document.getElementById('Drop-head').innerHTML =  category;
             break;
         case 2:
-            cli = "Categories : " + selected[v-1].innerHTML
-            document.getElementById('Drop-head').innerHTML =  cli;
+            category = "Categories : " + selected[v-1].innerHTML
+            document.getElementById('Drop-head').innerHTML =  category;
             break;
         case 3:
-            cli = "Categories : " + selected[v-1].innerHTML
-            document.getElementById('Drop-head').innerHTML =  cli;
+            category = "Categories : " + selected[v-1].innerHTML
+            document.getElementById('Drop-head').innerHTML =  category;
             break;
         case 4:
-            cli = "Categories : " + selected[v-1].innerHTML
-            document.getElementById('Drop-head').innerHTML =  cli;
+            category = "Categories : " + selected[v-1].innerHTML
+            document.getElementById('Drop-head').innerHTML =  category;
             break;
         case 5:
-            cli = "Categories : " + selected[v-1].innerHTML
-            document.getElementById('Drop-head').innerHTML =  cli;
+            category = "Categories : " + selected[v-1].innerHTML
+            document.getElementById('Drop-head').innerHTML =  category;
             break;
         case 6:
-            cli = "Categories : " + selected[v-1].innerHTML
-            document.getElementById('Drop-head').innerHTML =  cli;
+            category = "Categories : " + selected[v-1].innerHTML
+            document.getElementById('Drop-head').innerHTML =  category;
             break;
         
         default:
             break;
     }
+}
+
+function save(){
+    var amt = document.getElementById("amount").value;
+    category = document.getElementById('dropdown').innerText
+    console.log(category,amt);
+    if(localStorage.getItem("itemsJson")==null)
+    {
+        itemJsonArray=[]
+        itemJsonArray.push([category.slice(13,),amt])
+        localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray))
+    }
+    else{
+        itemstr=localStorage.getItem("itemsJson")
+        itemJsonArray = JSON.parse(itemstr)
+        itemJsonArray.push([category.slice(13,),amt])
+        localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray))
+    }
+
+
 }
