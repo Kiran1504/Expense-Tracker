d32b5d4bd5497d814d1cebc37458a0b42c2d5d99
Kiran1504 <117511419+Kiran1504@users.noreply.github.com>
2023-04-20 21:13:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,10 +13,9 @@ function changecategory(v){
     console.log(title);
 }
 
-function save(){
+function getandupdate(){
     var amt = document.getElementById("amount").value;
     category = document.getElementById('dropdown').innerText
-    console.log(category,amt);
     if(localStorage.getItem("itemsJson")==null)
     {
         itemJsonArray=[]
@@ -29,12 +28,47 @@ function save(){
         itemJsonArray.push([category.slice(13,),amt])
         localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray))
     }
+    save();
+}
 
-
+function save(){
+    if(localStorage.getItem("itemsJson")==null)
+    {
+        itemJsonArray=[]
+    }
+    else{
+        itemstr=localStorage.getItem("itemsJson")
+        itemJsonArray = JSON.parse(itemstr)
+    }
+    var data = localStorage.getItem('itemsJson')
     let tablebody = document.getElementById("table-body");
     let str = '';
     itemJsonArray.forEach((element , i) => {
-        str += '<tr><td> -- </td><td>'+  element[0] + ' </td><td>' + element[1] + ' </td></tr>';
+        str += '<tr><td> -- </td><td>'+  element[0] + ' </td><td>' + element[1] + ' </td><td><button class="delete-button" onclick="deleting('+i+')">Delete</button></td></tr>';
     });
     tablebody.innerHTML = str;
 }
+
+function clearing(){
+    localStorage.clear();
+    save()
+    
+}
+function deleting(i){
+    // console.log(i);
+    itemstr=localStorage.getItem("itemsJson");
+    itemJsonArray = JSON.parse(itemstr);
+    itemJsonArray.splice(i)
+    localStorage.setItem('itemsJson',JSON.stringify(itemJsonArray));
+    save();
+}
+const userdata = [
+    {
+        Grocery :{},
+        Medical:{},
+        Stationary:{}, 
+        Petrol:{}, 
+        Rent:{}, 
+        Miscelleneous:{}
+    }
+]
