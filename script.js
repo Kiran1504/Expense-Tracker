19fd524c7dde4c3ecd7cdc37d28702e92d532ebb
smitkhandelwal <smitkhandelwal0@gmail.com>
2023-06-04 17:05:33 +0530

    a little pizzaz to the save and clear buttons, colors pending

--- script.js
+++ script.js
@@ -234,7 +234,23 @@ function CheckLimit(){
     
 }
 
+var animateButton = function(e) {
 
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+  
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var classname = document.getElementsByClassName("save");
+  
+  for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', animateButton, false);
+  }
 //* A little table redesigning
 
 // function focustet(){
